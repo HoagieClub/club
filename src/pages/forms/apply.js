800f94d53d3c8aa7efcb45583294f59299c0d787
Hannah Choi <hannahchoi05@gmail.com>
2026-09-02 06:47:38 -0400

    fall 2026 application form

--- src/pages/forms/apply.js
+++ src/pages/forms/apply.js
@@ -3,6 +3,6 @@ import '../../styles/index.scss';
 import FormLayout from '../../components/formLayout';
 
 export default function Home() {
-  return <FormLayout name="Apply to Hoagie!" src="https://tally.so/embed/D4zXVl?alignLeft=1&transparentBackground=1">
+  return <FormLayout name="Apply to Hoagie!" src="https://tally.so/embed/KYjMMg?alignLeft=1&hideTitle=1&transparentBackground=1&dynamicHeight=1">
   </FormLayout>
 }
